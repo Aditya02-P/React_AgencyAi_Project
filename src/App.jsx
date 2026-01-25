e6c65e5d940f2f66c3b6e9870b3959413a4dc197
Aditya02-P <pradhanaditya0725@gmail.com>
2026-01-25 21:08:04 +0530

    Added final files

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,22 @@ import Services from './components/Services';
 import Ourwork from './components/Ourwork';
 import Teams from './components/Teams';
 import Contact from './components/Contact';
+import { Toaster } from 'react-hot-toast';
+import Footer from './components/Footer';
 const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'dark');
 
   return (
     <div className='dark:bg-black relative'>
       <Navbar theme={theme} setTheme={setTheme} />
+      <Toaster/>
       <Hero theme={theme}></Hero>
       <TrustedBy></TrustedBy>
       <Services></Services>
       <Ourwork></Ourwork>
       <Teams></Teams>
       <Contact ></Contact>
+      <Footer theme={theme}/>
     </div>
   )
 }
